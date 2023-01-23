883753b325654c29cfc237cd4f9d0a7560f82dfc
Kushal QA <kushal.parikh@tntra.io>
2023-01-23 17:46:00 +0530

    Dummytest added

--- src/main/java/com/qa/dummytest/Dummytest.java
+++ src/main/java/com/qa/dummytest/Dummytest.java
@@ -1,5 +1,24 @@
 package com.qa.dummytest;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.qa.utils.TestUtil;
+
 public class Dummytest {
+	public static WebDriver driver;
+
+	public static void main(String[] args) {
+		driver = new ChromeDriver(); 
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(TestUtil.PAGE_LOAD_TIMEOUT));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TestUtil.IMPLICIT_WAIT));
+		
+		driver.get("");
+
+	}
 
 }
