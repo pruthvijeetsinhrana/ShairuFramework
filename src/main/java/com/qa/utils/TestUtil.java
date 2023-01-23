dc56d900b01a6f29029811ffd893038bb2400e27
Kushal QA <kushal.parikh@tntra.io>
2023-01-23 18:59:20 +0530

    generic methods for menu,dropdown,click & is present added

--- src/main/java/com/qa/utils/TestUtil.java
+++ src/main/java/com/qa/utils/TestUtil.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 import com.qa.base.TestBase;
 
@@ -24,7 +25,9 @@ public class TestUtil extends TestBase {
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
 	}
 	
-	public static void navigate_to_destination(WebElement menu,WebElement submenu,WebElement submenu2) throws InterruptedException
+// It is used for 2nd level menu navigation
+	
+	public static void navigate_to_menu2(WebElement menu,WebElement submenu,WebElement submenu2) throws InterruptedException
 	{
 		Actions action = new Actions(driver);
 		action.moveToElement(menu).perform();
@@ -34,5 +37,39 @@ public class TestUtil extends TestBase {
 		action.moveToElement(submenu2).click().perform();
 		Thread.sleep(3000);
 	}
-
+	
+	// It is used for 1st level menu navigation
+	
+		public static void navigate_to_menu1(WebElement menu,WebElement submenu) throws InterruptedException
+		{
+			Actions action = new Actions(driver);
+			action.moveToElement(menu).perform();
+			Thread.sleep(2000);
+			action.moveToElement(submenu).perform();
+			Thread.sleep(2000);
+		}
+		
+		// It is used for dropdown selection
+		
+		public static void Dropdown_select(WebElement dropdown_element,String dropdown_text)
+		{
+			Select dropdown_option = new Select(dropdown_element);
+			dropdown_option.selectByVisibleText(dropdown_text);
+		}
+		
+		
+		// It is used for click on element
+		public static void ElementOnClick(WebElement click_element)
+		{
+			click_element.click();
+		}
+		
+		
+		// It is used for checking is element present
+		public static boolean IsElementPresent(WebElement present_element)
+		{
+			return present_element.isDisplayed();
+		}
+		
+		
 }
