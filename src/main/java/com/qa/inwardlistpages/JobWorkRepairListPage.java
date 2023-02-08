1f565cf03d040b8c144391bce6f06d4b61e065c7
Kushal Parikh <kushal.parikh@tntra.io>
2023-02-08 18:14:18 +0530

    added new page Job Work Repair page

---
+++ src/main/java/com/qa/inwardlistpages/JobWorkRepairListPage.java
@@ -0,0 +1,48 @@
+package com.qa.inwardlistpages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.qa.base.TestBase;
+import com.qa.utils.TestUtil;
+
+public class JobWorkRepairListPage extends TestBase{
+	
+	//Document Info section 
+	
+
+	
+		@FindBy(xpath= "(//span[normalize-space()='Inward List'])[1]")
+		WebElement inwardlist_menu;
+		
+		@FindBy(xpath= "(//a[normalize-space()='Job Work Repair List'])[1]")
+		WebElement jobworkrepairlist_menu;
+		
+		@FindBy(xpath= "(//span[@class='btn-text'])[1]")
+		WebElement inward_button;
+		
+		
+		public JobWorkRepairListPage()
+		{
+			PageFactory.initElements(driver, this);
+		}
+		
+		
+		public void Navigate_jobworkrepair_menu() 
+		{
+			try {
+				TestUtil.navigate_to_option1(inwardlist_menu,jobworkrepairlist_menu);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		public void Open_inward_button()
+		{
+			inward_button.isDisplayed();
+			inward_button.click();
+			System.out.println("Inward module is open - user can enter data in it.");
+		}
+}
